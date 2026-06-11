051a7cc438390033dcde1d049e9795356aac2287
Lucio Martinez <luciojmartinez-lab@users.noreply.github.com>
2026-06-11 20:45:45 +0200

    Release 100v4 restore desktop layout

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tiempos-pwa-100v3";
+const CACHE_NAME = "tiempos-pwa-100v4";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/styles.css",
-  "/app.js",
+  "/styles.css?v=100v4",
+  "/app.js?v=100v4",
   "/manifest.json",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
